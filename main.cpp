6cb795fda793330447c15eca8ed535a97979fae7
alec guinan <alecguinan@cis.vvc.edu>
2018-11-01 16:19:49 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,30 +1,33 @@
 #include<iostream>
 #include<string>
 #include "rational.h"
-Rational getrat(std::string prompt, std::string dprompt);
+Rational getrat(std::string nprompt, std::string dprompt);
 
 int main()
 {
-	Rational i = Rational(5, 2);
-	Rational n = Rational(7, 3);
+	Rational i = getrat("Please enter your numerator: ", "Please enter your denominator: ");
+	Rational n = getrat("Please enter your numerator: ", "Please enter your denominator: "); 
 	
 	Rational a = i.add(n);
-	
+	std::cout << "The numbers added are: ";
 	a.print();
 
 	Rational s = i.sub(n);
+	std::cout << "The numbers subtracted are: ";
 	s.print();
 
 	Rational d = i.div(n);
+	std::cout << "The numbers divided are: ";
 	d.print();
 
 	Rational m = i.mult(n);
+	std::cout << "The numbers multiplied are: ";
 	m.print();
 	
 	return 0;
 }
 
-Rational getrat(string nprompt , string dprompt)
+Rational getrat(std::string nprompt , std::string dprompt)
 {
 	std::cout << nprompt;
 	int n;
